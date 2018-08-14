9da23daf8b793006f941f35554ef1da5d83a815f
Matthias Gernand <m.gernand@fluxera.com>
2018-08-14 17:33:46 +0200

    Added test application solution.

---
+++ src/OpenBrisk.Runtime.Tester/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OpenBrisk.Runtime.Tester
+{
+	using System.Threading.Tasks;
+
+	internal static class Program
+    {
+	    private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
